2f9c25671a637d099747cbca16e59801b3bceb4b
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-26 17:41:01 +0900

    トップページ表示の実装

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,2 +1,5 @@
 class MypagesController < ApplicationController
+  def index
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root 'mypages#index'
 
-
-  
+  resources :mypages, only: :index
 end
