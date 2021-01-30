7e4946fd35c452ffb36f31af7c6f664c7a2dfdf2
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-30 14:07:29 +0900

    companyのバリデーションの記述

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,11 @@ class Company < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  with_options presence: true do
+    validates :company_name
+    validates :industry_id, numericality: { other_than: 1 }
+    validates :staff
+    validates :website
+  end
 end
--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -5,5 +5,5 @@ class Influencer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name_kanji,    presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' }
-  validates :phone_number,  presence: true  
+  validates :phone_number,  presence: true,  format: { with: /\A\d{10,11}\z/ }
 end
