69f776dee1a553a7efcaa96f266b61b873261803
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-30 13:44:08 +0900

    エラーの修正(2)

--- app/controllers/companies/omniauth_callbacks_controller.rb
+++ app/controllers/companies/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Companiess::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Companies::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
