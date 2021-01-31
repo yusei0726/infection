f7893b9f0b6dc791fc6617940a87162c645364ca
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-31 23:09:27 +0900

    mypageテーブルの生成

--- db/migrate/20210131140614_create_mypages.rb
+++ db/migrate/20210131140614_create_mypages.rb
@@ -3,6 +3,10 @@ class CreateMypages < ActiveRecord::Migration[6.0]
     create_table :mypages do |t|
 
       t.timestamps
+      t.string  :username,    null: false
+      t.string  :profile,     null: false
+      t.string  :url,         null: false
+      t.integer :category_id, null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_065206) do
+ActiveRecord::Schema.define(version: 2021_01_31_140614) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2021_01_28_065206) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_influencers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_influencers_on_reset_password_token", unique: true
+  end
+
+  create_table "mypages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username", null: false
+    t.string "profile", null: false
+    t.string "url", null: false
+    t.integer "category_id", null: false
   end
 
 end
