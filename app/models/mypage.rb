03c4506a093c521c59dba6a67dcee30ee4b5937e
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-31 23:16:32 +0900

    mypageのバリデーションの記述

--- app/models/mypage.rb
+++ app/models/mypage.rb
@@ -1,3 +1,11 @@
 class Mypage < ApplicationRecord
+
+  with_options presence: true do
+    validates :username
+    validates :profile
+    validates :url
+    validates :category_id, numericality: { other_than: 1 }
+  end
+
   belongs_to :influencer
 end
