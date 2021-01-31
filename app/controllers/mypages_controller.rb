3d9731f3cbe0e0e340b12739bdce0884d6539133
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-01-31 23:04:20 +0900

    newアクションのルーティング設定

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,4 +1,8 @@
 class MypagesController < ApplicationController
   def index
   end
+
+  def new
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
 
   root 'mypages#index'
 
-  resources :mypages, only: :index
+  resources :mypages, only: [:index, :new]
 end
