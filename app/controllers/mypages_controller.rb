4633f8e5164852b2723519892ab1e6fcc260b075
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-02-04 19:06:17 +0900

    controllers/mypagesのcreate,ストロングパラメーターの記述

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -5,4 +5,19 @@ class MypagesController < ApplicationController
   def new
     @mypage = Mypage.new
   end
+
+  def create
+    @mypage = Mypage.new(mypage_params)
+    if @mypage.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def mypage_params
+    params.require(:mypage).permit(:username, :profile, :url, :category_id, :image).merge(influencer_id: current_influencer.id)
+  end
 end
--- app/models/mypage.rb
+++ app/models/mypage.rb
@@ -1,12 +1,14 @@
 class Mypage < ApplicationRecord
 
+  belongs_to       :influencer
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+
   with_options presence: true do
     validates :username
     validates :profile
     validates :url
     validates :category_id, numericality: { other_than: 1 }
   end
-
-  belongs_to       :influencer
-  has_one_attached :image
 end
