c88b3c8316cc6fe63e7e692f90f387d1902a3446
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-02-04 22:39:02 +0900

    factories/mypagesの修正

--- spec/factories/mypages.rb
+++ spec/factories/mypages.rb
@@ -1,14 +1,15 @@
 FactoryBot.define do
   factory :mypage do
+
+    association :influencer
+
     username              { 'name' }
     profile               { 'describedescribedescribe' }
     url                   { 'http://localhost:3000/mypages/new.2' }
     category_id           { 2 }
-  end
-
-  association :influencer
 
     after(:build) do |mypage|
       mypage.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
     end
+  end
 end
