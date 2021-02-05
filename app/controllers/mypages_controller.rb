1e4d11c3c14c147b6c28f49d18a4a95ca89533d0
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2021-02-05 19:13:55 +0900

    mypages/showへのパス指定

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,4 +1,7 @@
 class MypagesController < ApplicationController
+
+  before_action :set_mypage, only: :show
+
   def index
     @mypages = Mypage.order('created_at DESC')
   end
@@ -17,12 +20,15 @@ class MypagesController < ApplicationController
   end
 
   def show
-    
   end
 
   private
 
   def mypage_params
     params.require(:mypage).permit(:username, :profile, :url, :category_id, :image).merge(influencer_id: current_influencer.id)
+  end
+
+  def set_mypage
+    @mypage = Mypage.find(params[:id])
   end
 end
